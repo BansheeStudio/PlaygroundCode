353bd71793ef4b6e9b8e0cbb77da2d1ab06291b8
BansheeStudio <113316414+BansheeStudio@users.noreply.github.com>
2023-12-05 17:51:24 +1100

    main

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello \nWorld");
+    //this is a comment
+    /*
+    printf("Hello \nWorld")
+    */
+    printf("Hello\nWorld\nHow\nIs\nIt\nGoing?\n");
+    printf("1\t2\t3\t4\t5");
     return(0);
 }
