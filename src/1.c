ce36e0fe7f151ba08b4d42d6522c1cdbd6ff30f4
BansheeStudio <113316414+BansheeStudio@users.noreply.github.com>
2023-12-06 13:49:37 +1100

    main

---
+++ src/1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int InvSlot1 = 1;
+    char name[] = "Bob";
+    char nickname = 'B';
+    char item[] = "Sticks";
+    int number = 14;
+    printf("hello my %s and i have %d %sin my inv slot %d also my nickname is %c",name,number,item,InvSlot1,nickname);
+    return(0);
+}
