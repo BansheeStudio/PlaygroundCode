3866b395f8a9c65f804e29865a082f55b3b44e50
Your Name <PlushBanshee000@gmail.com>
2024-01-04 19:09:55 +1100

    main

---
+++ crapcode/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int A = 8;
+    int B = 15;
+    int C = A+B;
+    printf("%d+%d=%dx63=.%d",A,B,C,C*63);
+    return(0);
+}
--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-int main()
-{
-    int A = 8;
-    int B = 15;
-    int C = A+B;
-    printf("%d+%d=%dx63=.%d",A,B,C,C*63);
-    return(0);
+#include <ctype.h>
+
+int main() {
+    char firstName[50], lastName[50];
+    int i;
+
+    printf("Enter your first name: ");
+    for (i = 0; i < 50 && isspace((unsigned char)getchar()); i++);
+    scanf("%s", firstName);
+
+    printf("Enter your last name: ");
+    for (i = 0; i < 50 && isspace((unsigned char)getchar()); i++);
+    scanf("%s", lastName);
+
+    printf("Your full name is %s %s\n", firstName, lastName);
+
+    return 0;
 }
