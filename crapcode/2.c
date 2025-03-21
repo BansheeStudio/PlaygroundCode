6824c672f10d778423d6a77b047cd38aa8a80b68
Your Name <PlushBanshee000@gmail.com>
2025-03-21 19:26:54 +1100

    .

--- crapcode/2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-int main()
-{
-time_t now;
-time(&now);
-printf("It's now %s\n",ctime(&now));
-return 0;
-}
--- crapcode/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int A = 8;
-    int B = 15;
-    int C = A+B;
-    printf("%d+%d=%dx63=.%d",A,B,C,C*63);
-    return(0);
-}
